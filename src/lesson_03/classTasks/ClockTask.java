b38696ee67ce8ed295d384d39638d3905c6c7ccb
AlexKomanov <52656401+AlexKomanov@users.noreply.github.com>
2023-03-13 19:11:38 +0200

    Adding ClockTask.java

---
+++ src/lesson_03/classTasks/ClockTask.java
@@ -0,0 +1,24 @@
+package lesson_03.classTasks;
+
+import java.util.Scanner;
+
+public class ClockTask {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("What time is it now? ");
+
+        int wholeTime = scanner.nextInt();
+
+        System.out.println("The current time is: ");
+        System.out.println((wholeTime / 100) + " : " + (wholeTime % 100));
+
+        int hours = wholeTime / 100;
+        int minutes = wholeTime % 100;
+
+        System.out.println("The current time is: ");
+        System.out.println(hours + " : " + minutes);
+
+    }
+}
