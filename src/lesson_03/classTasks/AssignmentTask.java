fe04297e918664d66578853c690372f32845daa7
AlexKomanov <52656401+AlexKomanov@users.noreply.github.com>
2023-03-13 20:34:43 +0200

    Update AssignmentTask.java

--- src/lesson_03/classTasks/AssignmentTask.java
+++ src/lesson_03/classTasks/AssignmentTask.java
@@ -5,14 +5,16 @@ public class AssignmentTask {
 
         int a = 10;
 
-        a += 90;
-        a -= 25;
-        a /= 3;
-        a %= 10;
-        a *= (-3);
-        a++;
-        a--;
+//        a += 90;
+//        a -= 25;
+//        a /= 3;
+//        a %= 10;
+//        a *= (-3);
+//        a++;
+//        a--;
 
         System.out.println("a = " + a);
+
+        System.out.println("a = " + (((((((a+90)-25)/3)%10)*-3)+1)-1));
     }
 }
