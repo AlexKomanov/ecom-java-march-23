264a42cf95c199c73db11fb786f244f0182a6cb2
AlexKomanov <52656401+AlexKomanov@users.noreply.github.com>
2023-03-13 19:54:14 +0200

    Create AssignmentOperators.java

---
+++ src/lesson_03/AssignmentOperators.java
@@ -0,0 +1,36 @@
+package lesson_03;
+
+public class AssignmentOperators {
+    public static void main(String[] args) {
+
+        //Optional declaration of variable
+        var a = 30;
+        System.out.println("a = " + a);
+        a = 50;
+
+        System.out.println("a = " + a);
+
+//        a = a + 5;
+        a += 5;
+        System.out.println("a = " + a);
+
+        a -= 10;
+        System.out.println("a = " + a);
+
+        a *= 2;
+        System.out.println("a = " + a);
+
+        a /= 2;
+        System.out.println("a = " + a);
+
+        a %= 6;
+        System.out.println("a = " + a);
+
+
+
+
+
+
+
+    }
+}
