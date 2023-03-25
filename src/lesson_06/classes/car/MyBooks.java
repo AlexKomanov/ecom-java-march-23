60eca87b87a4186820f40571cc4aab2257f831f9
AlexKomanov <52656401+AlexKomanov@users.noreply.github.com>
2023-03-25 16:21:23 +0300

    Adding Classes work

---
+++ src/lesson_06/classes/car/MyBooks.java
@@ -0,0 +1,10 @@
+package lesson_06.classes.car;
+
+public class MyBooks {
+    public static void main(String[] args) {
+
+        Book myBook = new Book();
+
+        Book newBook = new Book("Harry potter", 2015);
+    }
+}
